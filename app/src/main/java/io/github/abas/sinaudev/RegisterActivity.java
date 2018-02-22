4faae56d57cbba787db5f976e1c04152e6c29f56
abas <akhmadbasir5@gmail.com>
2018-02-22 17:26:15 +0700

    merge update

--- app/src/main/java/io/github/abas/sinaudev/RegisterActivity.java
+++ app/src/main/java/io/github/abas/sinaudev/RegisterActivity.java
@@ -1,18 +1,32 @@
 package io.github.abas.sinaudev;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public TextInputLayout name,email,password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        name = (TextInputLayout)findViewById(R.id.til_name);
+        email = (TextInputLayout)findViewById(R.id.til_email);
+        password =  (TextInputLayout)findViewById(R.id.til_password);
+
         ButterKnife.bind(this);
+
+        // lanjutkan di SDK @part II menit 11:03
+
 
     }
 }
