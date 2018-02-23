c9d44d783de2059d432fd919313972aebb0a952a
ahmadbasir <akhmadbasir5@gmail.com>
2018-02-23 09:49:43 +0700

    create service Api

--- app/src/main/java/io/github/abas/sinaudev/network/ApiService.java
+++ app/src/main/java/io/github/abas/sinaudev/network/ApiService.java
@@ -1,8 +1,22 @@
 package io.github.abas.sinaudev.network;
 
+import io.github.abas.sinaudev.entities.AccessToken;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 /**
  * Created by user on 23/02/2018.
  */
 
 public interface ApiService {
+
+    @POST("register")
+    @FormUrlEncoded
+    Call<AccessToken> register(
+            @Field("name") String name,
+            @Field("email") String email,
+            @Field("password") String password
+    );
 }
