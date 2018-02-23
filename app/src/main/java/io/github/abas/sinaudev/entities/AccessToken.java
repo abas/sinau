84b6e95d557dac7b15b0019f94d3a11772a89c5e
ahmadbasir <akhmadbasir5@gmail.com>
2018-02-23 09:48:57 +0700

    create helper to get AccessToken

--- app/src/main/java/io/github/abas/sinaudev/entities/AccessToken.java
+++ app/src/main/java/io/github/abas/sinaudev/entities/AccessToken.java
@@ -1,8 +1,15 @@
 package io.github.abas.sinaudev.entities;
 
+import com.squareup.moshi.Json;
+
 /**
  * Created by user on 23/02/2018.
  */
 
 public class AccessToken {
+    @Json(name = "token_type") String tokenType;
+    @Json(name = "expires_in") int expiresIn;
+    @Json(name = "access_token") String accessToken;
+    @Json(name = "refresh_token") String refreshToken;
+
 }
