20e6a3580c11e914658fb1da31a1fcf48ec21e1c
ahmadbasir <akhmadbasir5@gmail.com>
2018-02-23 09:49:15 +0700

    Auth Api error

--- app/src/main/java/io/github/abas/sinaudev/entities/ApiError.java
+++ app/src/main/java/io/github/abas/sinaudev/entities/ApiError.java
@@ -1,8 +1,21 @@
 package io.github.abas.sinaudev.entities;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by user on 23/02/2018.
  */
 
 public class ApiError {
+    String message;
+    Map<String, List<String>> errors;
+
+    public Map<String, List<String>> getErrors() {
+        return errors;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
