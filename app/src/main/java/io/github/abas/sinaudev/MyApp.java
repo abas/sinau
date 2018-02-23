27588aa16fb44732246a6d453adc4efb38c172dc
ahmadbasir <akhmadbasir5@gmail.com>
2018-02-23 09:46:56 +0700

    create new app MyApp

--- app/src/main/java/io/github/abas/sinaudev/MyApp.java
+++ app/src/main/java/io/github/abas/sinaudev/MyApp.java
@@ -1,8 +1,26 @@
 package io.github.abas.sinaudev;
 
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by user on 23/02/2018.
  */
 
-public class MyApp {
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+
+        if(LeakCanary.isInAnalyzerProcess(this)){
+            return;
+        }
+        LeakCanary.install(this);
+    }
+
+
 }
