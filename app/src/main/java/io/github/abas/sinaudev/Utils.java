fe4f3ea696d31e35fc03da54343f5976a52dc778
ahmadbasir <akhmadbasir5@gmail.com>
2018-02-23 09:46:24 +0700

    reconfig login

---
+++ app/src/main/java/io/github/abas/sinaudev/Utils.java
@@ -0,0 +1,8 @@
+package io.github.abas.sinaudev;
+
+/**
+ * Created by user on 23/02/2018.
+ */
+
+public class Utils {
+}
---
+++ app/src/main/java/io/github/abas/sinaudev/entities/AccessToken.java
@@ -0,0 +1,8 @@
+package io.github.abas.sinaudev.entities;
+
+/**
+ * Created by user on 23/02/2018.
+ */
+
+public class AccessToken {
+}
