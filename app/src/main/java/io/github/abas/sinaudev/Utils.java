70bcdb0d6fa5e7d0cb3d0c2123b6d45183a3bb3b
ahmadbasir <akhmadbasir5@gmail.com>
2018-02-23 09:48:23 +0700

    create helper ApiError

--- app/src/main/java/io/github/abas/sinaudev/Utils.java
+++ app/src/main/java/io/github/abas/sinaudev/Utils.java
@@ -1,8 +1,32 @@
 package io.github.abas.sinaudev;
 
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+
+import io.github.abas.sinaudev.entities.ApiError;
+import io.github.abas.sinaudev.network.RetrofitBuilder;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
 /**
  * Created by user on 23/02/2018.
  */
 
 public class Utils {
+
+    public static ApiError converErrors(ResponseBody response){
+        Converter<ResponseBody, ApiError> converter = RetrofitBuilder.getRetrofit()
+                .responseBodyConverter(ApiError.class,new Annotation[0]);
+
+        ApiError apiError = null;
+
+        try {
+            apiError = converter.convert(response);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return apiError;
+    }
+
+
 }
