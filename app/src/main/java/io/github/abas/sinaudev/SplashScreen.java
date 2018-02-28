ccdd9b3495bb3c1da7082c75e8ce5b08d76b97bb
ahmadbasir <akhmadbasir5@gmail.com>
2018-02-28 23:02:43 +0700

    update resource xml

--- app/src/main/java/io/github/abas/sinaudev/SplashScreen.java
+++ app/src/main/java/io/github/abas/sinaudev/SplashScreen.java
@@ -23,9 +23,9 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 finish();
             }
-        }, 3000L); //3000 L = 3 detik
+        }, 1500L); //3000 L = 3 detik
     }
 }
